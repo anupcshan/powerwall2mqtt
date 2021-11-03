7c6f2feb3c2b234e4eec9b567a95104342c6c7e0
Anup Chenthamarakshan <anupcshan@gmail.com>
2021-11-02 21:59:30 -0700

    Log excess solar power to MQTT
    Log power being sent back to the grid to MQTT. This can be fed as input
    to OpenEVSE to facilitate PV diversion.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/anupcshan/powerwall2mqtt
+
+go 1.17
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	golang.org/x/net v0.0.0-20211101193420-4a448f8816b3
+)
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"golang.org/x/net/publicsuffix"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Ltime)
+
+	powerwallIP := flag.String("powerwall-ip", "", "Powerwall IP")
+	password := flag.String("password", "", "Powerwall password")
+	pollingInterval := flag.Duration("poll-interval", 10*time.Second, "Polling interval")
+	brokerUrl := flag.String("broker", "", "Broker url (e.g., tcp://127.0.0.1:1883)")
+	gridPowerInverseTopic := flag.String(
+		"grid-inverse-topic",
+		"powerwall/excess_power",
+		"Topic to log inverse/negative of grid power to",
+	)
+
+	flag.Parse()
+
+	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{
+		Jar: jar,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+
+	var buf bytes.Buffer
+
+	loginReq := struct {
+		Username   string `json:"username"`
+		Password   string `json:"password"`
+		Email      string `json:"email"`
+		ForceSmOff bool   `json:"force_sm_off"`
+	}{
+		Username:   "customer",
+		Password:   *password,
+		Email:      "hello@example.com",
+		ForceSmOff: false,
+	}
+
+	if err := json.NewEncoder(&buf).Encode(&loginReq); err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Post(fmt.Sprintf("https://%s/api/login/Basic", *powerwallIP), "application/json", &buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.Copy(io.Discard, resp.Body)
+	_ = resp.Body.Close()
+
+	mqttClient := mqtt.NewClient(
+		mqtt.NewClientOptions().
+			AddBroker(*brokerUrl).
+			SetAutoReconnect(true),
+	)
+
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Error connecting to MQTT: %s", token.Error())
+	}
+
+	ticker := time.NewTicker(*pollingInterval)
+
+	for {
+		resp, err = client.Get(fmt.Sprintf("https://%s/api/meters/aggregates", *powerwallIP))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var metersResp map[string]struct {
+			InstantPower float64 `json:"instant_power"`
+		}
+
+		if err := json.NewDecoder(resp.Body).Decode(&metersResp); err != nil {
+			log.Fatal(err)
+		}
+
+		_ = resp.Body.Close()
+
+		log.Println(metersResp)
+		if v, ok := metersResp["site"]; ok {
+			token := mqttClient.Publish(*gridPowerInverseTopic, 0, false, fmt.Sprintf("%f", -v.InstantPower))
+			_ = token.Wait()
+			if err := token.Error(); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		<-ticker.C
+	}
+}
